158be7358fff978e2cb8241ff127530dea935735
Messef <85040583+Messef@users.noreply.github.com>
2023-10-09 19:56:45 -0400

    Configured the background and got a decent navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+'use client'
+import Link from "next/link"
+
 export default function Home() {
   return (
-    <body className = "">
-    <h1 className = "text-center border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 transition duration-1000 ease-in-out focus-within:bg-slate-700 outline-none">Hi </h1>
-    </body>
+    <>
+    <body>
+    <div  className = "mt-0 bg-cover bg-[url('https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/toNirvana-00Z/phpAGMF1q.jpeg')]" >
+      <header className = "mt-0">
+        <Link href = "/aboutus" className = "mt-4 text-[200%] text-blue-200 px-8 drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)] stroke-black">About Us</Link>
+        <Link href = "/membership" className = "text-[200%] text-blue-200 px-8">Membership</Link>
+        <Link href = "/tournaments" className = "text-[200%] text-blue-200 px-8">Classes & Tournaments</Link>
+      </header>
 
+    <h1 className = "align-middle text-[500%] text-blue-200 font-sans text-center outline-none h-screen flex items-center justify-center">Welcome to the Marlboro Chess Club! </h1>
+    
+    </div>
+    </body>
+</>
   )
 }
---
+++ src/app/tournaments/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+
+export default function Page() {
+  return (
+
+    <body  className = "bg-center bg-cover  h-screen flex items-center justify-center" style = {{backgroundImage:
+       'url("https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/toNirvana-00Z/phpAGMF1q.jpeg")',
+       width: "100%"
+    }}>
+    <h1 className = "align-middle text-[500%] text-blue-200 font-sans text-center outline-none">Welcome to the Marlboro Chess Club! </h1>
+    </body>
+
+  )
+}
