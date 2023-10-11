a2718f864d09f0a9722db922ddc26425ce32880b
Messef <85040583+Messef@users.noreply.github.com>
2023-10-10 22:56:28 -0400

    Made the transitions on the home page
    I shall configure the media queries next time

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -1,15 +1,24 @@
 'use client'
+import Link from "next/link"
 
-
-export default function Page() {
+export default function Home() {
   return (
+    <>
+    <body>
+    <div  className = "mt-0 bg-cover bg-[url('https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/toNirvana-00Z/phpAGMF1q.jpeg')]" >
+      <header className = "mt-0">
+        <Link href = "/aboutus" className = "mt-4 text-[200%] text-blue-200 px-8 drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)] no-underline hover:underline decoration-indigo-500 transition duration-1000 ">About Us</Link>
+        <Link href = "/membership" className = "text-[200%] text-blue-200 px-8 drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)] stroke-black">Membership</Link>
+        <Link href = "/tournaments" className = "text-[200%] text-blue-200 px-8 drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)] stroke-black">Classes & Tournaments</Link>
+      </header>
 
-    <body  className = "bg-center bg-cover  h-screen flex items-center justify-center" style = {{backgroundImage:
-       'url("https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/toNirvana-00Z/phpAGMF1q.jpeg")',
-       width: "100%"
-    }}>
-    <h1 className = "align-middle text-[500%] text-blue-200 font-sans text-center outline-none">Welcome to the Marlboro Chess Club! </h1>
+    <h1 className = "align-middle text-[500%] text-blue-200 font-sans text-center outline-none h-screen flex items-center justify-center">Welcome to the Marlboro Chess Club! </h1>
+    
+    </div>
+    <div>
+      <h1 className = "mt-4 text-[200%] text-blue-200 px-8 drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)]">Why Should I Register Today?</h1>
+    </div>
     </body>
-
+</>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,14 @@ export default function Home() {
     <body>
     <div  className = "mt-0 bg-cover bg-[url('https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/toNirvana-00Z/phpAGMF1q.jpeg')]" >
       <header className = "mt-0">
-        <Link href = "/aboutus" className = "mt-4 text-[200%] text-blue-200 px-8 drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)] stroke-black">About Us</Link>
-        <Link href = "/membership" className = "text-[200%] text-blue-200 px-8">Membership</Link>
-        <Link href = "/tournaments" className = "text-[200%] text-blue-200 px-8">Classes & Tournaments</Link>
+        <Link href = "/aboutus" className = "mt-4 text-[200%] text-blue-200 px-8 hover:text-blue-400 hover:text-[250%] hover:drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)] transition-all duration-[1000ms] ">About Us</Link>
+        <Link href = "/membership" className = "mt-4 text-[200%] text-blue-200 px-8 hover:text-blue-400 hover:text-[250%] hover:drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)] transition-all duration-[1000ms] ">Membership</Link>
+        <Link href = "/tournaments" className = "text-[200%] text-blue-200 px-8 hover:text-blue-400 hover:text-[250%] hover:drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)] transition-all duration-[1000ms] ">Classes & Tournaments</Link>
       </header>
+ <div className = "px-10">
+    <h1 className = "mt-[15%] block align-middle text-[500%] text-blue-200 font-sans text-center outline-none h-screen items-center justify-center">Welcome to the Marlboro Chess Club! <p className = " text-[35%]"> by Akshat Tewari and Rakesh Chandwani</p></h1>
 
-    <h1 className = "align-middle text-[500%] text-blue-200 font-sans text-center outline-none h-screen flex items-center justify-center">Welcome to the Marlboro Chess Club! </h1>
-    
+ </div>
     </div>
     </body>
 </>
