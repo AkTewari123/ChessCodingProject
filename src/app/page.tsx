1fde2937e31a1e9ef42edeed756a48f0d41da10f
Messef <85040583+Messef@users.noreply.github.com>
2023-10-16 12:27:31 -0400

    Make text gradient + text boxes + black background
    Make Webpage Dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,21 +5,42 @@ export default function Home() {
   return (
     <>
     <body className = "relative">
-    <header className = "flex flex-row md:flex-col mt-0 bg-slate-900 p-4 text-white text-[150%] ">
-        <Link href = "/aboutus" className = "text-white ml-[2%] font-sans hover:text-slate-400 transition-all duration-[500ms] md:text-center">About Us </Link>
-        <Link href = "/membership" className = "text-white ml-[5%] md:ml-[2%] font-sans hover:text-slate-400 transition-all duration-[500ms] md:text-center">Membership</Link>
-        <Link href = "/tournaments" className = "text-white ml-[5%] md:ml-[2%] font-sans hover:text-slate-400 transition-all duration-[500ms] md:text-center">Classes & Tournaments</Link>
+    <header className = "flex flex-row md:flex-col mt-0 bg-whitep-4 text-white text-[150%] ">
+        <Link href = "/aboutus" className = "text-black ml-[2%] font-sans hover:text-slate-400 transition-all duration-[500ms] md:text-center p-3">About Us </Link>
+        <Link href = "/membership" className = "text-black ml-[5%] md:ml-[2%] font-sans hover:text-slate-400 transition-all duration-[500ms] p-3 md:text-center">Membership</Link>
+        <Link href = "/tournaments" className = "text-black ml-[5%] md:ml-[2%] font-sans hover:text-slate-400 transition-all duration-[500ms] p-3 md:text-center">Classes & Tournaments</Link>
       </header>
     <div  className = "mt-0 relative bg-cover bg-center h-[30%] py-[5%] bg-[url('https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/PedroPinhata/phpVmOHiw.png')]" >
  <div className = "px-10">
     <h1 className = " align-middle text-[500%] text-white  font-sans text-center items-center justify-center">Welcome to the Marlboro Chess Club! <p className = " text-[35%]"> by Akshat Tewari and Rakesh Chandwani</p></h1>
     </div>
     </div>
-    <div className = "h-100 bg-black bg-cover">
+    <div className = "w-full align-center text-center justify-center text-[300%] text-white font-sans bg-black"><b>About Us</b></div>
+    <div className = "h-screen bg-black bg-cover">
     <div className="flex flex-row">
-  <div className="rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1 w-[20%] overflow-auto">
-    <div className="flex bg-black overflow-scroll">
-      <article className="text-[150%] font-sans text-white p-2 block align-center text-center">Sign up Right Now!<p className = "align-center text-center justify-center">fdl;ahglasdkgljf;lawhgk;dsjgkl;adshg;dsakl;gha;slfdska;lfhas;lk;a</p></article>
+  <div className="rounded-md bg-gradient-to-r from-blue-500 via-purple-500 to-green-500 p-1 w-[25%] overflow-auto mt-[10%] ml-[3%] h-fit">
+    <div className="flex bg-black overflow-auto text-center h-full">
+      <article className="text-[175%] font-sans bg-gradient-to-r from-blue-500 via-purple-500 to-green-500 inline-block text-transparent bg-clip-text p-[5%] block w-[100%] overflow-auto"><strong>Sign Up Today!</strong><p className = "align-center text-center justify-center text-[75%] text-white mt-[3%]">Sign up now for affordable chess tutoring 
+      to take your child to the 
+      next level! By getting tutoring from 
+      1500 USCF rated player Akshat Tewari, you get a custom plan perfect for your needs! With progress assessments, 
+      puzzles, game analysis, and endgame lessons, anyone rated 500 to 1200 can learn something!</p></article>
+    </div>
+  </div>
+  <div className="rounded-md bg-gradient-to-r from-red-500 via-pink-500 to-indigo-500 p-1 w-[25%] overflow-auto mt-[10%] ml-[10%] h-fit">
+    <div className="flex bg-black overflow-auto text-center h-full">
+      <article className="text-[175%] font-sans bg-gradient-to-r from-red-500 via-pink-500 to-indigo-500 inline-block text-transparent bg-clip-text p-[5%] block w-[100%] overflow-auto">
+        <strong>Our Story</strong><p className = "align-center text-center justify-center text-[75%] text-white mt-[3%]">
+          As a chess club, we started virtually as the pandemic commenced. Some kids wanted to make an official chess club as opposed to our virtual meetings after school ended. 
+          After the pandemic started to slow down and people were going back to in-person school, we decided to make an in-person club! We have been thriving ever since!</p></article>
+    </div>
+  </div>
+  <div className="rounded-md bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 p-1 w-[25%] overflow-auto mt-[10%] ml-[10%] h-fit">
+    <div className="flex bg-black overflow-auto text-center h-full">
+      <article className="text-[175%] font-sans bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 inline-block text-transparent bg-clip-text p-[5%] block w-[100%] overflow-auto">
+        <strong>How Do I Sign up?</strong><p className = "align-center text-center justify-center text-[75%] text-white mt-[3%]">
+          By texting 732-772-3931, you can express your interest in your chess club! Remember to cruise through the website to find out more about class dates and times! In addition, you can also sign up for private tutoring with Akshat for the $20 for 30 minutes deal! Have a nice day! We hope to see you in our chess club! 
+          </p></article>
     </div>
   </div>
 </div>
