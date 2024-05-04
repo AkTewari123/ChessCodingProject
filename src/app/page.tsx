7e870bb1fdc564a984db663f3fafbf7cd61399fc
Messef <85040583+Messef@users.noreply.github.com>
2024-05-04 16:43:34 -0400

    Things seem to get going decently

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,36 @@
 import Link from "next/link";
 
 export default function Home() {
-  const topImg = "absolute ml-[10%] mt-[20%] h-[15%] object-fill flex-auto";
+  var topImageHeight = 110;
+  var topImageWidth = topImageHeight / 2;
+  var chessBoardHeight = 50;
+  var margins = {};
+  var chessBoardWidth = chessBoardHeight / 2;
+  const chessBoardTopImage = `rotate-[345deg] absolute mt-[20%] mr-[60%] h-[${chessBoardHeight}%] w-[${chessBoardWidth}%] object-fill flex-auto`;
   const aboutUsHeader =
-    "text-[150%] text-white w-[150px] inline-block font-nunito p-2 h-[150%] border-b-2 border-white";
+    "text-[150%] text-white w-[125px] p-[2px] font-nunito  border-b-2 border-white";
   const otherHeaders =
-    "text-[150%] text-white font-nunito  ml-[2%] p-2 border-b-2 border-white h-[150%]";
-  const backgroundTop =
-    "justify-center bg-gradient-to-tr from-darkvi via-30% via-darkpurp via-lightorange via-80%  to-yell h-[500px] flex flex-wrap";
+    "text-[150%] text-white font-nunito  ml-[4%] p-[2px] border-b-2 border-white ";
+  const backgroundTop = `object-contain justify-center bg-gradient-to-tr from-darkvi via-30% via-darkpurp via-lightorange via-80%  to-yell h-[500px] flex flex-wrap h-screen`;
+  const headImage = `absolute h-[${topImageHeight}%] w-[${topImageWidth}%] float-left mr-[90%]`;
+  const greeting = `text-[350%] text-white w-[40%] text-center float-right mb-[30%] font-bold`;
+  const bodyC = `font-nunito`;
   const header =
-    "text-center relative place-content-center justify-center p-[10px] flex flex-row  h-[10%]";
+    "text-center mt-[1%] h-[5%] relative place-content-center font-semibold justify-center p-[1] flex flex-row  w-[100%]";
   return (
     <>
       <link
         href="https://fonts.googleapis.com/css?family=Nunito:400,700&display=swap"
         rel="stylesheet"
       />
-      <body>
+      <body className={bodyC}>
         <div className={backgroundTop}>
-          <div className={header}>
+          <header className={header}>
+            <img
+              className={headImage}
+              src="https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/PedroPinhata/phpkXK09k.png"
+              alt="Image Not Loading"
+            />
             <Link href="/" className={aboutUsHeader}>
               About Us
             </Link>
@@ -31,12 +43,13 @@ export default function Home() {
             <Link href="/membership" className={otherHeaders}>
               Classes
             </Link>
-          </div>
+          </header>
           <img
-            className={topImg}
-            src="https://images.chesscomfiles.com/uploads/v1/images_users/tiny_mce/NOooooooYuo/php4SF9w8.png"
+            className={chessBoardTopImage}
+            src="https://i0.wp.com/jkcreativewood.com/wp-content/uploads/2016/12/XL-CHESS-SET-DETAIL-scaled.jpg?fit=2560%2C2560&ssl=1"
             alt="Image Not Loading"
           />
+          <div className={greeting}>Welcome to the Marlboro Chess Club!</div>
         </div>
       </body>
     </>
